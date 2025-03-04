40eeed9b332b9ac79704286acc07d7e54e4948ca
alexonlyman <chuvakchel@mail.ru>
2025-03-04 18:37:51 +0300

    init

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM openjdk:21
 WORKDIR /app
 EXPOSE 8080
 COPY target/TaskManagement-0.0.1-SNAPSHOT.jar /app/TaskManagement-0.0.1-SNAPSHOT.jar
-ENTRYPOINT ["java", "-jar", "TaskManagement-0.0.1-SNAPSHOT.jar"]
+ENTRYPOINT ["java", "-jar", "/app/TaskManagement-0.0.1-SNAPSHOT.jar"]
--- src/main/java/alexgr/taskmanagement/config/SecurityConfig.java
+++ src/main/java/alexgr/taskmanagement/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorization ->
-                        authorization.anyRequest().permitAll()
+                        authorization
                                 .requestMatchers(ADMIN_LIST).hasRole("ADMIN")
                                 .requestMatchers(AUTH_WHITELIST).permitAll()
                                 .anyRequest().authenticated()
